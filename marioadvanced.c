a103dd33c3b90c1cdc21347073aaebbe2f7065f0
brcthn <burcu1818@gmail.com>
2020-08-26 11:02:39 +0200

    1 week - marioadvanced

---
+++ marioadvanced.c
@@ -0,0 +1,33 @@
+
+#include <cs50.h>
+#include <stdio.h>
+ void mario();
+int main(void)
+{
+   mario();
+}
+ void mario()
+{
+ int x;
+    do{
+       x=get_int("height:");
+    }
+   while(x<=0|| x>8);
+    for(int i=1;i<=x;i++) 
+  { 
+    for( int k=0;k<x-i;k++)
+    {
+      printf(" ");
+    }
+    for(int y=0;y<i;y++)
+    {
+        printf("*");
+    }
+     printf("  ");
+     for(int z=0;z<i;z++)
+    {
+        printf("*");
+    }
+     printf("\n");
+  }
+}
